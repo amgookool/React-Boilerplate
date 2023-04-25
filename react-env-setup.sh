490e53b7a470f2786ffcc31d98a2c6ff2ebe156c
Adrian Gookool <amgookool@hotmail.com>
2023-04-25 06:38:35 -0400

    Boilerplate Repository for React Framework

---
+++ react-env-setup.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+npm init -y
+
+# React and Dependencies Installation
+npm install react react-dom --save
+
+# Webpack Installation
+npm install webpack webpack-dev-server webpack-cli --save
+
+# Babel Installation 
+npm install @babel/core babel-loader @babel/preset-env @babel/preset-react  --save-dev
+
+# HTML Webpack Plugin Installation
+npm install html-webpack-plugin --save-dev
+
+# CSS Loader Installation
+npm install style-loader css-loader --save-dev
+
+# File Loader Installation
+npm install file-loader --save-dev
+
+
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+// Import Styles
+import "./Assets/Styles/App.css";
+
+//Import Components
+
+const App = () => (
+  <div>
+    <h1 className="my-style">Hello React!!!!!</h1>
+    <h2>Hello WOrld</h2>
+  </div>
+);
+
+export default App;
